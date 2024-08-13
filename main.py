bf9cf2dad29bb834096f97188efebd001d8585b7
renganomics <orengasi@gmail.com>
2024-08-13 14:28:57 +0100

    Day 29: Initial Commit

---
+++ main.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template
+import pandas
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/api/v1/<station>/<date>")
+def about(station, date):
+    temperature = 23
+    return {"station": station,
+            "date": date,
+            "temperature": temperature}
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
