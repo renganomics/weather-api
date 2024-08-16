249e2b9b2d653ca778197e75d781162928f9af44
renganomics <orengasi@gmail.com>
2024-08-16 10:32:46 +0100

    Day 31: Show stations table on the homepage

--- main.py
+++ main.py
@@ -3,10 +3,12 @@ import pandas as pd
 
 app = Flask(__name__)
 
+stations = pd.read_csv("data_small/stations.txt", skiprows=17)
+stations = stations[["STAID", "STANAME                                 "]]
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", data=stations.to_html())
 
 
 @app.route("/api/v1/<station>/<date>")
